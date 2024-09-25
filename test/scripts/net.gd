933c72514873446981a9b091c45181ffaa4c7865
Zy <frzysk@proton.me>
2024-09-25 21:36:52 +0200

    save: (test almost done)

--- test/scripts/net.gd
+++ test/scripts/net.gd
@@ -1,22 +1,62 @@
 extends Node
 
+var readyButton: bool = false
+var readyPrompt: bool = false
+
+func printCool(txt: String) -> void:
+	print("##### ", txt, " #####")
+
+func waitPrompt() -> String:
+	readyPrompt = true
+	var txt: String = await get_node("LineEdit").text_submitted
+	readyPrompt = false
+	printCool("Prompted: [" + txt + "]")
+	return txt
+
 func _ready() -> void:
-	var host = "some.noray.host"
+	var host = "tomfol.io"
 	var port = 8890
 	var err = OK
 
-	# Connect to noray
 	err = await Noray.connect_to_host(host, port)
 	if err != OK:
-		print("Error 1-", err) # Failed to connect
+		printCool("Error 1-" + String(err))
+		return
 
-	# Register host
 	Noray.register_host()
 	await Noray.on_pid
-
-	# Register remote address
-	# This is where noray will direct traffic
+	
 	err = await Noray.register_remote()
 	if err != OK:
-		print("Error 2-", err) # Failed to register
-	print("done :)")
+		printCool("Error 2-" + String(err))
+		return
+	printCool("Noray.local_port: " + str(Noray.local_port))
+	printCool("Noray.oid: " + String(Noray.oid))
+	printCool("[Click on a button]")
+	readyButton = true
+
+func on_choice(is_server: bool) -> void:
+	if not readyButton:
+		return
+	readyButton = false
+	if is_server:
+		printCool("SERVER MODE")
+		printCool("OID: " + Noray.oid)
+		var peer = ENetMultiplayerPeer.new()
+		var error = peer.create_server(Noray.local_port)
+		if error != OK:
+			printCool("Error 3-" + str(error))
+			return
+		printCool("OK")
+	else:
+		printCool("CLIENT MODE")
+		printCool("[Send OID]")
+		var oid: String = await waitPrompt()
+		# Connect using NAT punchthrough
+		var error: Error = Noray.connect_nat(oid)
+		if error != OK:
+			printCool("Error 4-" + str(error))
+			return
+		printCool("yay!!")
+		# Or connect using relay
+		#Noray.connect_relay(oid)
